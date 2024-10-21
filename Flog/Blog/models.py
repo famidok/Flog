bb6faedb4e5871ebb5d1d0d2eb77c23a382433b1
famidok <onder.emir@protonmail.com>
2024-10-21 12:06:53 +0300

    Create Post and Comment model and change admin.py

--- Flog/Blog/models.py
+++ Flog/Blog/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    author =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save() 
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length = 255)
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
+
+''' class User(models.Model):
+    name = models.CharField
+    surname = 
+    birthday =
+    job =
+    hobby = 
+    bio =
+'''
+
+'''class Tag(models.Model):
+    django-tagging
+'''
