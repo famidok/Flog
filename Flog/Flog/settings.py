0de5b546968d46a854cddfb6a8c68151e8a334b9
famidok <onder.emir@protonmail.com>
2024-10-22 09:38:15 +0300

    Fix settings.py for static file directory

--- Flog/Flog/settings.py
+++ Flog/Flog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c=$!sb10uyxta*8f3lt(!6or$zszo(v*#wl7gw31v+t+qtd92%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
