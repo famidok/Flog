32f9225039d3f0a23ccfc5975a665bd88fd6895d
famidok <onder.emir@protonmail.com>
2024-10-22 10:48:58 +0300

    Add HTML, CSS and function that depends on

--- Flog/Blog/views.py
+++ Flog/Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,7 @@ def post_list(request):
         'posts' : posts
     }
     return render(request, 'post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
